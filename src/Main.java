fa8e071dd50ed3daa73e25fa2f117366d99b2aa1
olensis <olensis7gmail@.com>
2022-09-21 10:52:34 +0300

    все готово)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        // Задание 1
+    String firstName ="Ivan";
+    String middleName = "Ivanovich";
+    String lastName = "Ivanov";
+    String fullName = firstName + " "+  middleName + " " + lastName;
+        System.out.println("Фио Сотрудника " + fullName);
+
+        // Задание 2
+
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —" + fullNameUpper);
+
+        // Задание 3
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replace("ё", "е");
+        System.out.println(" Данные ФИО сотрудника — " + fullName1 );
+}
+
+
 }
